5b41c076fa825c664d17835444739e045c3827f6
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-25 22:53:38 -0400

    fixing cards

---
+++ pseudoscript.js
@@ -0,0 +1,28 @@
+//global variables
+searchHistory={}
+apiUrl
+apiKey
+
+//reference DOM elements - ex document.queryselector, etc - containers and document elements
+
+
+
+//function to display search history from user input in local storage
+    //allow access to city name when it is clicked (grab city  name from local storage of previous search)
+
+
+
+//create a function to retrieve search history from local storage
+    var cardHead = document.createElement("div") //create elements 
+
+
+//function to display current weather data and append information to page
+
+//create second api call to retrieve five day forecast and render to page
+
+//make a call function to get UV index + render to page (get latitude/longitude from one api, and plug it in to get the UV)
+    //create variables to grab api response to be able to search results into second api call
+
+//create a handler to check search fields and return data - if no data is in search field, return :/do nothing
+
+
