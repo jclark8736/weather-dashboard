dcecf6c085f6eed614f0df64f82cb818a987aad0
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-28 09:50:51 -0400

    calls working for all but uv

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,11 +33,11 @@ let test;
 
 
 //day 1
-var dayOneWeather;
-var dayOneTemp;
-var dayOneHumid;
-var dayOneWind;
-var dayOneTime;
+let dayOneWeather;
+let dayOneTemp;
+let dayOneHumid;
+let dayOneWind;
+let dayOneTime;
 
 
 //day 2
@@ -97,12 +97,50 @@ function getForecast() {
         console.log(locRes)
         forecastRaw = locRes;
         //day 1
-        var dayOneWeather = forecastRaw.list[0].weather[0].main
-        var dayOneTemp = forecastRaw.list[0].main.temp
-        var dayOneHumid = forecastRaw.list[0].main.humidity
-        var dayOneWind =  forecastRaw.list[0].wind.speed
-        var dayOneTime = forecastRaw.list[0].dt
-        console.log(dayOneTime)
+        let dayOneWeather = forecastRaw.list[0].weather[0].main
+        let dayOneTemp = forecastRaw.list[0].main.temp
+        let dayOneHumid = forecastRaw.list[0].main.humidity
+        let dayOneWind =  forecastRaw.list[0].wind.speed
+        let dayOneTime = forecastRaw.list[0].dt
+        console.log (dayOneTime)
+
+
+
+        //day 2
+        let dayTwoWeather = forecastRaw.list[8].weather[0].main
+        let dayTwoTemp = forecastRaw.list[8].main.temp
+        let dayTwoHumid = forecastRaw.list[8].main.humidity
+        let dayTwoWind =  forecastRaw.list[8].wind.speed
+        let dayTwoTime = forecastRaw.list[8].dt
+        console.log (dayTwoTime)
+
+
+        //day 3
+        let dayThreeWeather = forecastRaw.list[16].weather[0].main
+        let dayThreeTemp = forecastRaw.list[16].main.temp
+        let dayThreeHumid = forecastRaw.list[16].main.humidity
+        let dayThreeWind =  forecastRaw.list[16].wind.speed
+        let dayThreeTime = forecastRaw.list[16].dt
+        console.log (dayThreeTime)
+
+        // day 4
+        let dayFourWeather = forecastRaw.list[24].weather[0].main
+        let dayFourTemp = forecastRaw.list[24].main.temp
+        let dayFourHumid = forecastRaw.list[24].main.humidity
+        let dayFourWind =  forecastRaw.list[24].wind.speed
+        let dayFourTime = forecastRaw.list[24].dt
+        console.log (dayFourTime)
+
+
+        //day five
+
+        let dayFiveWeather = forecastRaw.list[32].weather[0].main
+        let dayFiveTemp = forecastRaw.list[32].main.temp
+        let dayFiveHumid = forecastRaw.list[32].main.humidity
+        let dayFiveWind =  forecastRaw.list[32].wind.speed
+        let dayFiveTime = forecastRaw.list[32].dt
+        console.log (dayFiveTime)
+
 
 
       });
